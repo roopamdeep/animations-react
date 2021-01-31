43020d9710137a228cf4e3fc6a1f8489d329e78a
Roopamdeep Kaur <roopam_kaur@yahoo.in>
2021-01-31 18:51:17 -0500

    added my first text animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import React, { Component } from "react";
+import { easePolyOut } from "d3-ease";
+import Animate from "react-move/Animate";
+class App extends Component {
+  state = {};
+  onAnimate = () => (
+    <Animate
+      show={true}
+      start={{
+        opacity: 0,
+        fontSize: 0,
+        rotate: 0,
+      }}
+      enter={{
+        opacity: [1],
+        fontSize: [50],
+        rotate: [360],
+        timing: { duration: 2000, ease: easePolyOut },
+      }}
+    >
+      {({ opacity, rotate, fontSize }) => {
+        return (
+          <div
+            style={{
+              position: "absolute",
+              opacity: opacity,
+              fontSize: fontSize,
+              color: "teal",
+              rotate: rotate,
+            }}
+          >
+            Hello World!
+          </div>
+        );
+      }}
+    </Animate>
   );
+  render() {
+    return <div>{this.onAnimate()}</div>;
+  }
 }
 
 export default App;
